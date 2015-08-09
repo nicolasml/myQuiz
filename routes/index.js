11134edada1f63efbbbf342441549704e23ad7ea
Nicolás Muñoz <nicolas.profe@gmail.com>
2015-08-09 11:31:22 +0200

    Primera pregunta :-)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+// instanciamos controlador Primera pregunta
+var qC = require('../controllers/quiz_controller');
+var quizController = qC(); 
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+// Primera pregunta
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
