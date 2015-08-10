6e67a4bd22397b25e248c6bcb7dfa300d6f285d9
Nicolás Muñoz <nicolas.profe@gmail.com>
2015-08-10 16:13:58 +0200

    Credenciales en rama creditos

--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,9 @@ router.get('/', function(req, res) {
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
+// Autor
+router.get('/author', function(req, res) {
+  res.render('author');
+});
+
 module.exports = router;
